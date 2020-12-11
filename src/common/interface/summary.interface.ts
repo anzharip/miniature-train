9e548249007366f77edc06a258da25cb85ba4613
anzharip <10259593+anzharip@users.noreply.github.com>
2020-12-11 11:58:18 +0700

    fix: removing unneeded interface

--- src/common/interface/summary.interface.ts
+++
@@ -1,7 +0,0 @@
-import { google } from "@google-cloud/language/build/protos/protos";
-
-export interface Summary {
-  wordFrequency: [string, number][];
-  replyToStatusId: string;
-  sentiment: google.cloud.language.v1.IAnalyzeSentimentResponse;
-}
